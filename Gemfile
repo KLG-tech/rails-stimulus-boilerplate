e687e5d6b98cfe70fe7fb92f5ec95f7f7459f741
Kelvin <kelvin.ith@kawanlamacorp.com>
2025-07-11 17:26:42 +0700

    Merge branch 'main' into step-6/file-attachments-with-s3

--- Gemfile
+++ Gemfile
@@ -57,6 +57,10 @@ gem 'omniauth-rails_csrf_protection'
 gem 'omniauth-keycloak'
 gem 'aws-sdk-s3', require: false
 
+gem 'rolify'
+gem 'pundit'
+
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,19 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
     def configure_permitted_parameters
       # For sign up
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+  private
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referer || root_path)
     end
 end
