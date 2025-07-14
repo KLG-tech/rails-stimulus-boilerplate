ed60371501293b26abb97ad045efe92e6d442a0e
Kelvin <kelvin.ith@kawanlamacorp.com>
2025-07-14 10:39:22 +0700

    chore: add basic auth for mission control jobs ui and remove configurations for queue table

--- app/controllers/mission_control_jobs_controller.rb
+++ app/controllers/mission_control_jobs_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class MissionControlJobsController < ApplicationController
-  before_action :authenticate_user!
+  http_basic_authenticate_with(
+    name: ENV.fetch("BASIC_AUTH_USERNAME"),
+    password: ENV.fetch("BASIC_AUTH_PASSWORD")
+  )
+
+  def index
+  end
 end
